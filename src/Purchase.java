2676185f4a34113cb3293f90e801268d3a6c4be2
Nikita Platonov <fifaakkaynt92@gmail.com>
2023-05-10 17:57:10 +0300

    исправил метод sum в класс Purchase

--- src/Purchase.java
+++ src/Purchase.java
@@ -28,13 +28,17 @@ public class Purchase {
 
     public long sum(Map<String, Integer> prices) {
         long sum = 0;
-        System.out.println("КОРЗИНА:");
-        for (int i = 0; i < purchases.length; i++) {
-            Purchase purchase = purchases[i];
+        for (Purchase purchase : purchases) {
             if (purchase == null) continue;
-            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + (purchase.count * prices.get(purchase.title)) + " руб.");
-            sum += purchase.count * prices.get(purchase.title);
+            sum += (long) purchase.count * prices.get(purchase.title);
         }
         return sum;
+    }
+    public void showingTheBasket(Map<String, Integer> prices){
+        System.out.println("КОРЗИНА:");
+        for (Purchase purchase : purchases) {
+            if (purchase == null) continue;
+            System.out.println("\t" + purchase.title + " " + purchase.count + " шт. в сумме " + (purchase.count * prices.get(purchase.title)) + " руб.");
+        }
     }
 }
